1895cd9b27cf6f1d0039c1d3f57ea8c53c410d91
Drew <dandadrew02@gmail.com>
2023-04-15 14:43:20 -0500

    added file

---
+++ testhing3.py
@@ -0,0 +1,18 @@
+from amadeus import Client, ResponseError
+
+amadeus = Client(
+    client_id='XgxDwEAknQAEA0YVcGZW8MNoG8ZyTBBX',
+    client_secret='eBQCujYHCw79WFpK'
+)
+
+try:
+    response = amadeus.shopping.flight_offers_search.get(
+        originLocationCode='MAD',
+        destinationLocationCode='ATH',
+        departureDate='2023-11-01',
+        adults=1)
+    print(response.data)
+
+    
+except ResponseError as error:
+    print(error)
